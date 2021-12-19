6dfbbd882fea202f75a02a3b80369c3aab36587e
Agus Acosta <agustinacosta301999@gmail.com>
2021-12-19 05:37:57 -0300

    Agregada funcionalidad para calcular la edad menor, mayor y promedio, para luego mostrarlas en pantalla. Arreglado bug que creaba mas inputs de los necesarios.

--- primer-tarea/primer-tarea.js
+++ primer-tarea/primer-tarea.js
@@ -7,6 +7,10 @@ const $integrantesFamilia = document.querySelector("#integrantesFamilia")
 const $camposPorIntegranteFamiliar = document.querySelector("#camposPorIntegranteFamiliar")
 const $botonAceptar = document.querySelector("#botonAceptar")
 const $botonReiniciar = document.querySelector("#botonReiniciar")
+const $botonEnviar = document.querySelector("#botonEnviar")
+const $strongMenorEdad = document.querySelector("#strongMenorEdad")
+const $strongMayorEdad = document.querySelector("#strongMayorEdad")
+const $strongPromedioEdades = document.querySelector("#strongPromedioEdades")
 
 $botonAceptar.onclick = function() {
     if (Number($integrantesFamilia.value) <= 0) return
@@ -15,17 +19,30 @@ $botonAceptar.onclick = function() {
     crearInputsParaIntegrantes(numeroIntegrantes)
     mostrarElemento($camposPorIntegranteFamiliar)
     esconderElemento($botonAceptar)
+    mostrarElemento($botonEnviar)
     mostrarElemento($botonReiniciar)
 
     return false;
 }
 
 $botonReiniciar.onclick = function() {
+    resetearTexto($strongMenorEdad)
+    resetearTexto($strongMayorEdad)
+    resetearTexto($strongPromedioEdades)
     eliminarInputsParaIntegrantes()
+    esconderElemento($botonEnviar)
     esconderElemento($botonReiniciar)
     mostrarElemento($botonAceptar)
 
     return false;
+}
+
+$botonEnviar.onclick = function() {
+    mostrarMenorEdad(calcularMenorEdad())
+    mostrarMayorEdad(calcularMayorEdad())
+    mostrarPromedioEdades(calcularPromedioEdades())
+
+    return false
 }
 
 function obtenerNumeroIntegrantes() {
@@ -33,7 +50,7 @@ function obtenerNumeroIntegrantes() {
 }
 
 function crearInputsParaIntegrantes(numeroIntegrantes) {
-    for (let i = 0; i <= numeroIntegrantes; i++) {
+    for (let i = 1; i <= numeroIntegrantes; i++) {
         $camposPorIntegranteFamiliar.innerHTML += 
         `<label for="edadIntegrante${i}Familia">Edad integrante:</label>
          <br>
@@ -43,14 +60,72 @@ function crearInputsParaIntegrantes(numeroIntegrantes) {
     }
 }
 
-function eliminarInputsParaIntegrantes() {
-    $camposPorIntegranteFamiliar.innerHTML = ""
-}
-
 function mostrarElemento($elemento) {
     $elemento.className = ""
 }
 
 function esconderElemento($elemento) {
     $elemento.className = "hidden"
+}
+
+function resetearTexto($elemento) {
+    $elemento.textContent = ""
+}
+
+function eliminarInputsParaIntegrantes() {
+    $camposPorIntegranteFamiliar.innerHTML = ""
+}
+
+function calcularMenorEdad() {
+    let menorEdad = 0
+
+    for (const childNode of $camposPorIntegranteFamiliar.childNodes) {
+        if (childNode.localName === "input" && Number(childNode.value) > 0) {
+            if (menorEdad > Number(childNode.value)) {
+                menorEdad = Number(childNode.value)
+            }
+        }
+    }
+
+    return menorEdad
+}
+
+function calcularMayorEdad() {
+    let mayorEdad = 0
+
+    for (const childNode of $camposPorIntegranteFamiliar.childNodes) {
+        if (childNode.localName === "input" && Number(childNode.value) > 0) {
+            if (mayorEdad < Number(childNode.value)) {
+                mayorEdad = Number(childNode.value)
+            }
+        }
+    }
+
+    return mayorEdad
+}
+
+function calcularPromedioEdades() {
+    let promedioEdades = 0
+    let cantidadIntegrantes = 0
+
+    for (const childNode of $camposPorIntegranteFamiliar.childNodes) {
+        if (childNode.localName === "input" && Number(childNode.value) > 0) {
+            promedioEdades += Number(childNode.value)
+            cantidadIntegrantes++
+        }
+    }
+
+    return promedioEdades / cantidadIntegrantes
+}
+
+function mostrarMenorEdad(menorEdad) {
+    $strongMenorEdad.textContent = `La menor edad es: ${menorEdad}.`
+}
+    
+function mostrarMayorEdad(mayorEdad) {
+    $strongMayorEdad.textContent = `La mayor edad es: ${mayorEdad}.`
+}
+    
+function mostrarPromedioEdades(promedioEdades) {
+    $strongPromedioEdades.textContent = `El promedio de edades es: ${promedioEdades}.`
 }
