d68911db07155f32aec3d73b7e07b380023a8a51
Agus Acosta <agustinacosta301999@gmail.com>
2021-12-19 05:08:00 -0300

    Agregada funcionalidad para crear inputs y labels, de acuerdo al numero de integrantes ingresado por el usuario. Ademas, al pulsar posteriormente el boton reiniciar estos nuevos inputs y label seran eliminados, permitiendo al usuario reiniciar el proceso

--- primer-tarea/primer-tarea.js
+++ primer-tarea/primer-tarea.js
@@ -2,3 +2,55 @@
 Crear tantos inputs+labels como gente haya para completar la edad de cada integrante.
 Al hacer click en "calcular", mostrar en un elemento pre-existente la mayor edad, la menor edad y el promedio del grupo familiar.
 Punto bonus: Crear un botón para "empezar de nuevo" que empiece el proceso nuevamente, borrando los inputs ya creados (investigar cómo en MDN). */
+
+const $integrantesFamilia = document.querySelector("#integrantesFamilia")
+const $camposPorIntegranteFamiliar = document.querySelector("#camposPorIntegranteFamiliar")
+const $botonAceptar = document.querySelector("#botonAceptar")
+const $botonReiniciar = document.querySelector("#botonReiniciar")
+
+$botonAceptar.onclick = function() {
+    if (Number($integrantesFamilia.value) <= 0) return
+
+    const numeroIntegrantes = obtenerNumeroIntegrantes()
+    crearInputsParaIntegrantes(numeroIntegrantes)
+    mostrarElemento($camposPorIntegranteFamiliar)
+    esconderElemento($botonAceptar)
+    mostrarElemento($botonReiniciar)
+
+    return false;
+}
+
+$botonReiniciar.onclick = function() {
+    eliminarInputsParaIntegrantes()
+    esconderElemento($botonReiniciar)
+    mostrarElemento($botonAceptar)
+
+    return false;
+}
+
+function obtenerNumeroIntegrantes() {
+    return Number($integrantesFamilia.value)
+}
+
+function crearInputsParaIntegrantes(numeroIntegrantes) {
+    for (let i = 0; i <= numeroIntegrantes; i++) {
+        $camposPorIntegranteFamiliar.innerHTML += 
+        `<label for="edadIntegrante${i}Familia">Edad integrante:</label>
+         <br>
+         <input type="number" id="edadIntegrante1Familia">
+         <br>
+         <br>`
+    }
+}
+
+function eliminarInputsParaIntegrantes() {
+    $camposPorIntegranteFamiliar.innerHTML = ""
+}
+
+function mostrarElemento($elemento) {
+    $elemento.className = ""
+}
+
+function esconderElemento($elemento) {
+    $elemento.className = "hidden"
+}
